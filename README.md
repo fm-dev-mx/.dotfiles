65b41bc92e37a54d6d1f11be121b8e11914e0884
fm-dev-mx <frgmendoza@gmail.com>
2022-06-10 21:03:25 -0600

    config files for i3 fedora 36

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# .dotfiles
-Personal dotfiles configuration
+# i3-dot-files
+Dot files for i3wm
---
+++ create-symlinks.sh
@@ -0,0 +1,8 @@
+
+#!/bin/zsh
+mkdir -p ~/Pictures
+ln -s $HOME/.config/i3-dot-files/config/alacritty ~/.config/alacritty
+ln -s $HOME/.config/i3-dot-diles/config/autorandr/ ~/.config/autorandr
+ln -s $HOME/.config/i3-dot-files/config/i3 ~/.config/i3
+ln -s $HOME/.config/i3-dot-files/config/polybar ~/.config/polybar
+ln -s $HOME/.config/i3-dot-files/background.jpg ~/Pictures/background.jpg
