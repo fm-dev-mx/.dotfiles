5aff23f4fb7aaf837c377a84724e11df7b3ef81e
fm dev mx <fm.dev.mx@gmail.com>
2022-06-13 03:09:02 -0600

    config update, now with awesome instead of i3

--- create-symlinks.sh
+++ create-symlinks.sh
@@ -1,8 +1,30 @@
-
 #!/bin/zsh
-mkdir -p ~/Pictures
-ln -s $HOME/.config/i3-dot-files/config/alacritty ~/.config/alacritty
-ln -s $HOME/.config/i3-dot-diles/config/autorandr/ ~/.config/autorandr
-ln -s $HOME/.config/i3-dot-files/config/i3 ~/.config/i3
-ln -s $HOME/.config/i3-dot-files/config/polybar ~/.config/polybar
-ln -s $HOME/.config/i3-dot-files/background.jpg ~/Pictures/background.jpg
+#############################################################################
+#									    #
+# ### NOT IN USE, ONLY FOR FUTURES REFERENCES ###############################
+# 									    #
+#  ln -s $HOME/.config/i3-dot-files/config/alacritty ~/.config/alacritty    #
+#  ln -s $HOME/.config/i3-dot-files/config/i3 ~/.config/i3                  #
+
+
+#  mkdir -p ~/Pictures
+#  ln -s $HOME/.config/i3-dot-files/background.jpg ~/Pictures/background.jpg
+#                                                                           #
+#############################################################################
+#
+#  ln -s $HOME/.config/i3-dot-diles/config/autorandr/ ~/.config/autorandr     
+#  ln -s $HOME/.config/i3-dot-files/config/polybar ~/.config/polybar
+   
+#  Oh-my-zsh
+#  ln -s $HOME/.dotfiles/config/.oh-my-zsh $HOME/
+#  ln -s $HOME/.dotfiles/config/.zshrc $HOME/
+#  rm  $HOME/.zsh_history
+#  ln -s $HOME/.dotfiles/config/.zsh_history $HOME/
+#
+#
+#  Brosers
+   ln -s $HOME/.dotfiles/config/browsers/BraveSoftware $HOME/.config/
+   ln -s $HOME/.dotfiles/config/browsers/google-chrome $HOME/.config/  
+   ln -s $HOME/.dotfiles/config/browsers/google-chrome-beta $HOME/.config/
+#
+#
